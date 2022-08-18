a44c65dae8677595be8ee2de7a2e8dac3b961d40
Brian Marete <bmarete10@gmail.com>
2022-08-18 16:11:14 +0300

    Dockerize app

--- server/server.js
+++ server/server.js
@@ -15,8 +15,8 @@ const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
 	useNewUrlParser: true,
 	useCreateIndex: true,
-	useUnifiedTopology: true }
-);
+	useUnifiedTopology: true
+});
 
 mongoose.connection.once('open', () => {
 	console.log("MongoDB database connection established successfully.");
